47a5aaae9d064572243db1e435aaf9e4e5ab01b7
Gabriel Trindade <gabrieltrindade.nave@gmail.com>
2023-05-29 20:55:01 -0300

    Update Home.jsx
    aqui modifiquei o lorem impsum para um texto que realmente conversa com o intuito do nosso site :)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,8 +13,30 @@ export default function Home() {
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-black">Crie notas para melhor controle e etc.</h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-black-400">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elit lorem, convallis eu metus rhoncus, luctus tristique sem. Proin eget aliquet purus, eget lobortis nisl. Vivamus ex nunc, malesuada nec tortor sed, sollicitudin pretium nisi. Nam sollicitudin molestie neque, tempor aliquet nibh molestie at. Nam elementum justo ut risus mattis faucibus. Aenean scelerisque a lectus quis tristique. Pellentesque at varius sapien. Donec est neque, interdum in molestie vitae, fringilla id lorem. Maecenas id vehicula dolor. Praesent ultricies odio eget augue finibus, eget ultrices dolor semper. Quisque sagittis mi ut vestibulum vulputate. Phasellus condimentum dolor eget diam vulputate placerat ut eu erat.
+            
+            Bem-vindo ao nosso site de criação e gerenciamento de anotações! Aqui, você pode liberar sua criatividade e organizar suas ideias em arquivos de anotação personalizados.
             </p>
+
+            <p>
+            Com nossa interface intuitiva e fácil de usar, você pode criar anotações com facilidade, formatar o texto, adicionar imagens e até mesmo categorizá-las em diferentes pastas para uma organização eficiente.
+            </p>
+
+            <p>
+            Nossa plataforma permite que você acesse suas anotações de qualquer lugar, a qualquer momento, usando qualquer dispositivo. Nunca mais perca suas ideias brilhantes!
+            </p>
+
+            <p>
+            Além disso, oferecemos recursos avançados de pesquisa e filtro para ajudá-lo a localizar rapidamente suas anotações específicas. Com apenas alguns cliques, você encontrará a informação que precisa.
+            </p>
+
+            <p>
+            Compartilhe suas anotações com colegas de equipe, amigos ou familiares para colaboração e feedback. Trabalhe em conjunto para tornar suas ideias ainda mais poderosas!
+            </p>
+
+            <p>
+            Seja você um estudante, profissional criativo ou alguém que adora manter suas ideias organizadas, nosso site de criação e gerenciamento de anotações está aqui para ajudar. Comece a explorar sua criatividade e a manter suas ideias sempre à mão. Experimente agora e veja como é fácil anotar e gerenciar suas ideias de forma eficiente!
+            </p>
+
             <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Crie notas
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
