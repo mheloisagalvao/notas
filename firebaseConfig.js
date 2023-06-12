80b83572aa73417be1101ed4e46e9a92c9d5a793
Gabriel Trindade <gabrieltrindade.nave@gmail.com>
2023-06-12 18:52:34 -0300

    configuração do firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import "firebase/compat/database";
+import 'firebase/compat/auth'
 
 const firebaseConfig = {
-};
-
+  apiKey: "AIzaSyBdgCZqpWUj73YFiEt1OEUr8YmLUSSurDY",
+  authDomain: "mynotes-9ecf8.firebaseapp.com",
+  projectId: "mynotes-9ecf8",
+  storageBucket: "mynotes-9ecf8.appspot.com",
+    messagingSenderId: "974761909870",
+  appId: "1:974761909870:web:9fb9f87e651df2d2e1107f"
+}
 firebase.initializeApp(firebaseConfig);
 
-export default firebaseConfig;
+export const app = firebase.app();
+export default firebase
